0da032fe266fdba0dc95d8d5fcabf2840454a22f
Charlie Angriawan <charlieangriawan@outlook.com>
2018-07-11 03:00:06 +0800

    Test IOS Web App

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,21 @@ var cards = new Vue({
   }
 })
 
+$(document).ready(function() {
+  // iOS web app full screen hacks.
+  if (window.navigator.standalone == true) {
+    // make all link remain in web app mode.
+    $('a').click(function() {
+      window.location = $(this).attr('href');
+      return false;
+    });
+  }
+});
+
+
 $('#ocbc_card').bind('click touchstart', function() {
+  // window.scrollTo(0,1);
   document.body.requestFullscreen();
 }, false);
 
-// window.scrollTo(0,1);
+
