b38ae1e756ba0a9c9c4e1372962f0966fd3ca8c3
Charlie Angriawan <charlieangriawan@outlook.com>
2018-07-11 20:13:24 +0800

    Hidden Address Bar

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ function normalizePort(val) {
 var port = normalizePort(process.env.PORT || '80');
 
 //run website
-var app = express()
+var app = express();
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/src/main.html');
-})
+});
 app.get('/gum', function (req, res) {
     res.sendFile(__dirname + '/src/gum.html');
-})
+});
+
 app.use(express.static(__dirname + '/src'));
 const server = http.createServer(app);
 server.listen(port, function listening() {
--- src/js/main.js
+++ src/js/main.js
@@ -27,3 +27,12 @@ $('#button_wallet').bind('click touchstart', function() {
   $('#page_wallet').show();
 });
 
+/* iOS re-orientation fix */
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+  /* iOS hides Safari address bar */
+  window.addEventListener("load",function() {
+      setTimeout(function() {
+          window.scrollTo(0, 1);
+      }, 1000);
+  });
+}
