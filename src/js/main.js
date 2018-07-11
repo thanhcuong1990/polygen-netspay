d667098232f9e869860dd36a1df1e2597f72a767
Charlie Angriawan <charlieangriawan@outlook.com>
2018-07-11 20:27:09 +0800

    Partial Fullscreen

--- src/js/main.js
+++ src/js/main.js
@@ -27,12 +27,23 @@ $('#button_wallet').bind('click touchstart', function() {
   $('#page_wallet').show();
 });
 
-/* iOS re-orientation fix */
-if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
-  /* iOS hides Safari address bar */
-  window.addEventListener("load",function() {
-      setTimeout(function() {
-          window.scrollTo(0, 1);
-      }, 1000);
+var attachMobileSafariAddressBarHelpTip = function (target) {
+  var $target = $(target);
+  $target.tooltip({
+      title: 'Scroll up to hide Safari address bar',
+      trigger: 'manual',
+      placement: 'bottom'
   });
+  $(window).on('resize', function () {
+      var bodyHeight = document.body.offsetHeight;
+      var windowHeight = window.innerHeight;
+      var isLandscape = Math.abs(window.orientation) === 90;
+      var showTooltip = (windowHeight < bodyHeight);
+      if(!isLandscape) return;
+      $target.tooltip(showTooltip ? 'show' : 'hide');
+  });
+}
+var ua = window.navigator.userAgent;
+if(ua.indexOf('iPhone') !== -1 && ua.indexOf('Safari') !== -1) {
+  attachMobileSafariAddressBarHelpTip('#main-nav');
 }
