2373155123d864bd5ae712e2feed44f482ded9c5
Charlie Angriawan <charlieangriawan@outlook.com>
2018-07-24 19:01:56 +0800

    window.backbutton

--- src/js/main.js
+++ src/js/main.js
@@ -92,25 +92,10 @@ $('#amount_input').on('keydown', function(event) {
   }, 10);
 });
 
-window.addEventListener('popstate', function(event) {
-  // The popstate event is fired each time when the current history entry changes.
-  // var r = confirm("You pressed a Back button! Are you sure?!");
-  window.alert('popstate');
-  if (r == true) {
-      // Call Back button programmatically as per user confirmation.
-      // history.back();
-      // Uncomment below line to redirect to the previous page instead.
-      // window.location = document.referrer // Note: IE11 is not supporting this.
-  } else {
-      // Stay on the current page.
-      // history.pushState(null, null, window.location.pathname);
+$(window).backbutton(function(ev){
+  window.alert('yes');
+  if (ev.which === 8) {
+      goBack();
+      ev.preventDefault();
   }
-
-  history.pushState(null, null, window.location.pathname);
-
-}, false);
-
-window.onhashchange = function() {
-  //blah blah blah
-  window.alert('hashchanged');
- }
+});    
